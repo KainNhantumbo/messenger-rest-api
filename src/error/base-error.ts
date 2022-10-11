2bf6ca3d2201e3aa4330cd4394183078809e8222
Kain Nhantumbo <nhantumbok@gmail.com>
2022-10-11 21:01:03 +0200

    dev: added custom error class

---
+++ src/error/base-error.ts
@@ -0,0 +1,11 @@
+
+export default class GenericError extends Error {
+  public readonly message: string;
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super();
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
