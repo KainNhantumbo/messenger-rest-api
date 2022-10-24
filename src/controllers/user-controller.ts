0eb4a0c54aedff33b73ad7346ac647c2720b4d93
Kain Nhantumbo <nhantumbok@gmail.com>
2022-10-24 21:35:04 +0200

    feat: created delete and update user functionanity

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,26 +1,29 @@
 import AppError from '../error/base-error';
 import UserModel from '../models/User';
+import * as bcrypt from 'bcrypt';
 import { Request as IReq, Response as IRes } from 'express';
 
 export default class UserController {
-  async getUser(req: IReq, res: IRes) {
+  async getUser(req: IReq, res: IRes): Promise<void> {
     const userId = req.body.user;
     const user = await UserModel.findOne({ _id: userId })
-      .select('-password')
+      .select('-password -recovery_key')
       .lean();
 
     if (!user) throw new AppError('User not found.', 404);
     res.status(200).json({ user });
   }
 
-  async getAllUsers(req: IReq, res: IRes) {
-    const users = await UserModel.find({}).select('-password').lean();
+  async getAllUsers(req: IReq, res: IRes): Promise<void> {
+    const users = await UserModel.find({})
+      .select('-password -recovery_key')
+      .lean();
     res.status(200).json({ users });
   }
 
-  async createUser(req: IReq, res: IRes) {
+  async createUser(req: IReq, res: IRes): Promise<void> {
     const { password, email, ...data } = req.body;
-    if (!password || password.length < 6)
+    if (!password || String(password).length < 6)
       throw new AppError('Password must have at least 6 characters', 400);
     if (!email) throw new AppError('Please provide your e-mail adress', 400);
 
@@ -31,5 +34,38 @@ export default class UserController {
 
     const createdUser = await UserModel.create({ password, email, ...data });
     res.status(201).json({ userKey: createdUser.recovery_key });
+  }
+
+  async updateUser(req: IReq, res: IRes): Promise<void> {
+    var { password, user: userId, ...data } = req.body;
+    // check if user exists
+    const isUser = await UserModel.exists({ _id: userId }).lean();
+    if (!isUser) throw new AppError('User not found', 404);
+
+    if (password) {
+      if (String(password).length < 6)
+        throw new AppError('Password must have at least 6 characters', 400);
+
+      const salt = await bcrypt.genSalt(10);
+      password = await bcrypt.hash(password, salt);
+      await UserModel.updateOne(
+        { _id: userId },
+        { ...data, password },
+        { runValidators: true }
+      ).lean();
+    } else {
+      await UserModel.updateOne(
+        { _id: userId },
+        { ...data },
+        { runValidators: true }
+      ).lean();
+    }
+    res.sendStatus(200);
+  }
+
+  async deleteUser(req: IReq, res: IRes): Promise<void> {
+    const userId = req.body.user;
+    await UserModel.deleteOne({ _id: userId }).lean();
+    res.sendStatus(200);
   }
 }
