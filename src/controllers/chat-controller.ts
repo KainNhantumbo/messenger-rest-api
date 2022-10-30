72d6d249c4c4a0ab4cf73a3b04d9f82c8aa12440
Kain Nhantumbo <nhantumbok@gmail.com>
2022-10-30 22:55:44 +0200

    feat: added support querying all chats for the user.

--- src/controllers/chat-controller.ts
+++ src/controllers/chat-controller.ts
@@ -7,10 +7,7 @@ import UserModel from '../models/User';
 import MessageModel from '../models/Message';
 
 export default class ChatController {
-  async getChat(
-    req: IReq,
-    res: IRes
-  ): Promise<IRes<any, Record<string, any>> | undefined> {
+  async getChat(req: IReq, res: IRes) {
     const chatId = req.params.id;
     const userId = req.body.user;
     if (!chatId) throw new AppError('Chat ID must be provided.', 400);
@@ -40,28 +37,55 @@ export default class ChatController {
     return res.status(200).json({ ...data, messages, avatar: '' });
   }
 
-  async getAllChats(req: IReq, res: IRes): Promise<void> {
+  async getAllChats(req: IReq, res: IRes) {
     const userId = req.body.user;
-    const foundChats = await ChatModel.find({ author: userId })
-      .populate({ path: 'messages', options: { sort: 'createdAt' } })
-      .lean();
+    const foundChats = await ChatModel.find({
+      $or: [{ author: userId }, { friend: userId }],
+    }).lean();
 
-    //   const chats: any = foundChats.map((chat) => {
-    //     if (chat.picture && existsSync(chat.friend.picture?.filePath)) {
-    //       const avatarFileData = readFileSync(chat.friend.picture.filePath, {
-    //         encoding: 'base64',
-    //       });
-    //       const avatar = `data:image/${chat.friend.picture.extension};base64,${avatarFileData}`;
-    //       delete (chat.friend as any).picture;
-    //       (chat.friend as any).avatar = avatar;
-    //       return chat;
-    //     }
-    //     delete (chat as any).picture;
-    //     (chat as any).avatar = '';
-    //     return chat;
-    //   });
-    //   res.status(200).json({ chats });
-    // res.status(200).json({ ...chats });
+    if (foundChats.length === 0) return res.status(200).json(foundChats);
+
+    const transformChats = async (chat: any) => {
+      const [friendId] = [chat.author, chat.friend].filter(
+        (id) => id != userId
+      );
+      const message = (await MessageModel.find({ chatId: chat._id }).lean())
+        .sort((a: any, b: any) => (a.createdAt > b.createdAt ? 1 : -1))
+        .pop();
+      const foundUser = await UserModel.findOne({ _id: friendId })
+        .select('user_name picture')
+        .lean();
+      if (!foundUser) throw new AppError('User not found.', 404);
+      const { picture } = foundUser;
+      if (picture?.filePath && existsSync(picture?.filePath)) {
+        const avatarFileData = await readFile(picture.filePath, {
+          encoding: 'base64',
+        });
+
+        return {
+          _id: chat._id,
+          user_name: foundUser.user_name,
+          message,
+          avatar: `data:image/${picture.extension};base64,${avatarFileData}`,
+          createdAt: (chat as any).createdAt,
+        };
+      }
+      return {
+        _id: chat._id,
+        user_name: foundUser.user_name,
+        message,
+        avatar: '',
+        createdAt: (chat as any).createdAt,
+      };
+    };
+    const dataArr: any = [];
+    await Promise.allSettled(foundChats.map(transformChats)).then(
+      (result: any) => {
+        dataArr.push(result[0].value);
+      }
+    );
+
+    res.status(200).json(dataArr);
   }
 
   async createChat(
